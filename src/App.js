7473942a1cced272d2f45eb02639098948a98ecf
Khai Pham <khai.pnp@outlook.com>
2021-12-05 23:47:18 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/User/Home'
+import Management from './pages/User/Management'
 
 function App() {
 	return (
 		<div>
-			test <span>test</span>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/management' element={<Management />} />
+			</Routes>
 		</div>
 	)
 }
--- src/pages/User/Management/index.js
+++ src/pages/User/Management/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from '../Navbar'
 
-const Home = () => {
+const Management = () => {
 	return (
 		<div>
 			<Navbar />
-			<h1>Home</h1>
+			<h1>Management</h1>
 		</div>
 	)
 }
 
-export default Home
+export default Management
