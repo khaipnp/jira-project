138749831a3bebe0e1eca8f095dfd8f68135bd3c
Khai Pham <khai.pnp@outlook.com>
2021-12-07 01:17:04 +0700

    update register

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/User/Home'
 import Management from './pages/User/Management'
+import Register from './pages/User/Register'
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/management' element={<Management />} />
+				<Route path='/register' element={<Register />} />
 			</Routes>
 		</div>
 	)
